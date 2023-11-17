fda4c8c81e0908289a3de59eb13829cdae69fde4
Justin Ball <justinball@gmail.com>
2023-11-16 20:59:48 -0700

    Added missing exports

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ export { handleJwks } from './handlers/jwks';
 export { handleDynamicRegistrationInit } from './handlers/dynamic_registration_init';
 export { handleDynamicRegistrationFinish } from './handlers/dynamic_registration_finish';
 export { default as html } from './html/html';
+
+export { getJWKs, setJWKs, deleteJWKs, getCurrentKey, getCurrentPrivateKey, getKeySets, rotateKeys, getCurrentJwks } from './models/jwks';
+export { setOIDC, getOIDC, deleteOIDC } from './models/oidc';
+export { getPlatform, setPlatform, deletePlatform } from './models/platforms';
+export { getRemoteJWKs, setRemoteJWKs, deleteRemoteJWKs } from './models/remote_jwks';
