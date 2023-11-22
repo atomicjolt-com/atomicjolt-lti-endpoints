eb73f90366f56ad12232ebf5af58b4cafe83cd3a
Justin Ball <justinball@gmail.com>
2023-11-22 14:52:30 -0700

    built types and update version

--- types/handlers/deep_link.d.ts
+++ types/handlers/deep_link.d.ts
@@ -1,4 +1,11 @@
 import type { Context } from 'hono';
 export declare const deepLinkVersion = "1.3.0";
+export declare enum ContentItem {
+    File = "file",
+    HtmlFragment = "html",
+    Image = "image",
+    Link = "link",
+    LTIResourceLink = "ltiResourceLink"
+}
 export declare function handleSignDeepLink(c: Context): Promise<Response>;
 //# sourceMappingURL=deep_link.d.ts.map
